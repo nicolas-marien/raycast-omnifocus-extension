4629a0cb5de3b00726a4d3a37f05ef224d859699
Nicolas Marien <mariennicolas@icloud.com>
2024-11-30 14:40:52 +0100

    fix: only list non completed tasks in inbox

--- src/lib/list-tasks.ts
+++ src/lib/list-tasks.ts
@@ -1,15 +1,23 @@
 import { executeScript } from "./executeScript";
 export async function listTasks() {
-  return await executeScript(`
- const omnifocus = Application('OmniFocus');
-    const doc = omnifocus.defaultDocument;
-    const tasks = doc.inboxTasks();
-    
-    return tasks.map(task => ({
-        id: task.id(),
-        name: task.name(),
-        completed: task.completed(),
-        flagged: task.flagged()
-    }));
+  return await executeScript(/* javascript */ `
+const omnifocus = Application("OmniFocus");
+const doc = omnifocus.defaultDocument;
+
+const tasks = doc.inboxTasks();
+
+return tasks.reduce((ts, t) => {
+  const completed = t.completed();
+  if (!completed) {
+    ts.push({
+      id: t.id(),
+      name: t.name(),
+      completed,
+      flagged: t.flagged(),
+    });
+  }
+  return ts;
+}, []);
+
 `);
 }
