ea1cbaf6ef85c99ffd58929830246bc1f13724cc
Nicolas Marien <mariennicolas@icloud.com>
2024-12-08 20:32:45 +0100

    fix: filter undefined values

--- src/lib/add-task.ts
+++ src/lib/add-task.ts
@@ -5,19 +5,18 @@ import { CreateOmniFocusTaskOptions } from "./types";
 function getCreateTaskAppleScript(options: CreateOmniFocusTaskOptions, projectName?: string): string {
   const taskProperties = `
     name: '${options.name}',
-    flagged: ${options.flagged},
     ${options.deferDate ? "deferDate: new Date('" + options.deferDate.toISOString() + "')," : ""}
     ${options.dueDate ? "deferDate: new Date('" + options.dueDate + "')," : ""}
-    note: '${options.note}'
-  `;
+    ${options.note ? "note: '${options.note}'," : ""}
+    ${options.flagged ? "flagged: true," : ""}
+    `;
 
   if (!projectName) {
-    // Add to inbox
     return `
       const omnifocus = Application('OmniFocus');
       const doc = omnifocus.defaultDocument();
       
-      const task = omnifocus.Task({
+      const task = omnifocus.InboxTask({
         ${taskProperties}
       })
       doc.inboxTasks.push(task)
@@ -51,6 +50,8 @@ function getCreateTaskAppleScript(options: CreateOmniFocusTaskOptions, projectNa
 
 export async function addTask(options: CreateOmniFocusTaskOptions, projectName?: string): Promise<OmniFocusTask> {
   const script = getCreateTaskAppleScript(options, projectName);
+
+  console.log(script);
   const task = await executeScript(script);
   return task;
 }
