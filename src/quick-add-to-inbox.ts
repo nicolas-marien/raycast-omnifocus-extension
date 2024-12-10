1dfe2abd32e3f1c0ef907ec3b1e5d5cb205aec14
Nicolas Marien <mariennicolas@icloud.com>
2024-12-10 22:01:39 +0100

    chore: check if installed in quickadd

--- src/quick-add-to-inbox.ts
+++ src/quick-add-to-inbox.ts
@@ -1,9 +1,14 @@
 import { LaunchProps, showToast, Toast } from "@raycast/api";
 import { addTask } from "./lib/api/add-task";
+import { checkOmniFocusInstalled } from "./lib/utils/ensure-installed";
 
 export default async function AddTodoCommand(props: LaunchProps<{ arguments: Arguments.QuickAddToInbox }>) {
   const { todo } = props.arguments;
-  await addTask({ name: todo });
+  const isInstalled = await checkOmniFocusInstalled();
+  if (!isInstalled) {
+    return;
+  }
+  await addTask({ name: todo, tags: [] });
   await showToast({
     title: "Task created!",
     style: Toast.Style.Success,
