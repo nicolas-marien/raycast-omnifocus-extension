b65709aea04de2d852f8254d5c3602f4f978c926
Nicolas Marien <mariennicolas@icloud.com>
2024-12-11 21:37:52 +0100

    feat: refetch inbox after performing mutations

--- src/list-inbox.tsx
+++ src/list-inbox.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { OmniFocusTask } from "./lib/types/task";
-import { Action, ActionPanel, Icon, List } from "@raycast/api";
+import { Action, ActionPanel, Icon, List, showToast, Toast } from "@raycast/api";
 import { listTasks } from "./lib/api/list-tasks";
 import { open } from "@raycast/api";
 import { deleteTask } from "./lib/api/delete-task";
@@ -10,30 +10,57 @@ export default function ListInboxTasks() {
   const [tasks, setTasks] = useState<OmniFocusTask[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
+  const fetchTasks = async () => {
     setIsLoading(true);
-    listTasks().then((tasks) => {
-      setTasks(tasks);
-      setIsLoading(false);
-    });
+    const newTasks = await listTasks();
+    setTasks(newTasks);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTasks();
   }, []);
 
   return (
     <List isLoading={isLoading}>
-      {tasks.map((t) => (
-        <List.Item
-          key={t.id}
-          title={t.name}
-          icon={t.flagged ? Icon.Flag : undefined}
-          actions={
-            <ActionPanel>
-              <Action title="Open" onAction={() => open(`omnifocus:///task/${t.id}`)} icon={Icon.Eye} />
-              <Action title="Complete" onAction={() => completeTask(t.id)} icon={Icon.CheckCircle} />
-              <Action title="Delete" onAction={() => deleteTask(t.id)} icon={Icon.Trash} />
-            </ActionPanel>
-          }
-        />
-      ))}
+      {tasks.length === 0 && <List.EmptyView title="No tasks in inbox" />}
+      {tasks.length > 0 &&
+        tasks.map((t) => (
+          <List.Item
+            key={t.id}
+            title={t.name}
+            icon={t.flagged ? Icon.Flag : undefined}
+            actions={
+              <ActionPanel>
+                <Action title="Open" onAction={() => open(`omnifocus:///task/${t.id}`)} icon={Icon.Eye} />
+                <Action
+                  title="Complete"
+                  onAction={async () => {
+                    await completeTask(t.id);
+                    await showToast({
+                      title: "Task completed!",
+                      style: Toast.Style.Success,
+                    });
+                    await fetchTasks();
+                  }}
+                  icon={Icon.CheckCircle}
+                />
+                <Action
+                  title="Delete"
+                  onAction={async () => {
+                    await deleteTask(t.id);
+                    await showToast({
+                      title: "Task deleted!",
+                      style: Toast.Style.Success,
+                    });
+                    await fetchTasks();
+                  }}
+                  icon={Icon.Trash}
+                />
+              </ActionPanel>
+            }
+          />
+        ))}
     </List>
   );
 }
