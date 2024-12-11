1c02a436c5d87d28f93e96eae21bec54323cbf0c
Nicolas Marien <mariennicolas@icloud.com>
2024-12-11 21:28:02 +0100

    feat: add actions to complete or delete inbox tasks

--- src/list-inbox.tsx
+++ src/list-inbox.tsx
@@ -3,6 +3,8 @@ import { OmniFocusTask } from "./lib/types/task";
 import { Action, ActionPanel, Icon, List } from "@raycast/api";
 import { listTasks } from "./lib/api/list-tasks";
 import { open } from "@raycast/api";
+import { deleteTask } from "./lib/api/delete-task";
+import { completeTask } from "./lib/api/complete.task";
 
 export default function ListInboxTasks() {
   const [tasks, setTasks] = useState<OmniFocusTask[]>([]);
@@ -25,7 +27,9 @@ export default function ListInboxTasks() {
           icon={t.flagged ? Icon.Flag : undefined}
           actions={
             <ActionPanel>
-              <Action title="Open" onAction={() => open(`omnifocus:///task/${t.id}`)} />
+              <Action title="Open" onAction={() => open(`omnifocus:///task/${t.id}`)} icon={Icon.Eye} />
+              <Action title="Complete" onAction={() => completeTask(t.id)} icon={Icon.CheckCircle} />
+              <Action title="Delete" onAction={() => deleteTask(t.id)} icon={Icon.Trash} />
             </ActionPanel>
           }
         />
