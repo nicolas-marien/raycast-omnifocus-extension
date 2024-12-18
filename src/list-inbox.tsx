13933441391f44952a00fbf38eed436b42e041f4
Nicolas Marien <mariennicolas@icloud.com>
2024-12-18 10:42:47 +0100

    feat: enhance inbox list

--- src/list-inbox.tsx
+++ src/list-inbox.tsx
@@ -25,42 +25,49 @@ export default function ListInboxTasks() {
     <List isLoading={isLoading}>
       {tasks.length === 0 && <List.EmptyView title="No tasks in inbox" />}
       {tasks.length > 0 &&
-        tasks.map((t) => (
-          <List.Item
-            key={t.id}
-            title={t.name}
-            icon={t.flagged ? Icon.Flag : undefined}
-            actions={
-              <ActionPanel>
-                <Action title="Open" onAction={() => open(`omnifocus:///task/${t.id}`)} icon={Icon.Eye} />
-                <Action
-                  title="Complete"
-                  onAction={async () => {
-                    await completeTask(t.id);
-                    await showToast({
-                      title: "Task completed!",
-                      style: Toast.Style.Success,
-                    });
-                    await fetchTasks();
-                  }}
-                  icon={Icon.CheckCircle}
-                />
-                <Action
-                  title="Delete"
-                  onAction={async () => {
-                    await deleteTask(t.id);
-                    await showToast({
-                      title: "Task deleted!",
-                      style: Toast.Style.Success,
-                    });
-                    await fetchTasks();
-                  }}
-                  icon={Icon.Trash}
-                />
-              </ActionPanel>
-            }
-          />
-        ))}
+        tasks.map((t) => {
+          const accessories: List.Item.Accessory[] = [];
+          if (t.flagged) {
+            accessories.push({ icon: Icon.Flag });
+          }
+          return (
+            <List.Item
+              key={t.id}
+              title={t.name}
+              icon={!t.completed ? Icon.Circle : Icon.Checkmark}
+              accessories={accessories}
+              actions={
+                <ActionPanel>
+                  <Action title="Open" onAction={() => open(`omnifocus:///task/${t.id}`)} icon={Icon.Eye} />
+                  <Action
+                    title="Complete"
+                    onAction={async () => {
+                      await completeTask(t.id);
+                      await showToast({
+                        title: "Task completed!",
+                        style: Toast.Style.Success,
+                      });
+                      await fetchTasks();
+                    }}
+                    icon={Icon.CheckCircle}
+                  />
+                  <Action
+                    title="Delete"
+                    onAction={async () => {
+                      await deleteTask(t.id);
+                      await showToast({
+                        title: "Task deleted!",
+                        style: Toast.Style.Success,
+                      });
+                      await fetchTasks();
+                    }}
+                    icon={Icon.Trash}
+                  />
+                </ActionPanel>
+              }
+            />
+          );
+        })}
     </List>
   );
 }
