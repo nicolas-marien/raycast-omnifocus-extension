cb0fdc8188f369cbd9a6fe0888632f07f0be29c9
Nicolas Marien <mariennicolas@icloud.com>
2025-02-14 17:28:34 +0100

    feat: list tasks inside perspectives (#2)
    * chore(deps): update raycast api
    
    * chore: add perspective api
    
    * refactor: create task list component
    
    * wip: add perspective list command
    
    * feat: differentiate custom and built-in perspectives

---
+++ src/lib/api/list-perspectives-tasks.ts
@@ -0,0 +1,31 @@
+import { OmniFocusTask } from "../types/task";
+import { executeScript } from "../utils/executeScript";
+
+export async function listPerspectiveTasks(perspectiveName?: string): Promise<OmniFocusTask[]> {
+  if (!perspectiveName) {
+    return []
+  }
+  return await executeScript(`
+const omnifocus = Application('OmniFocus');
+const document = omnifocus.defaultDocument();
+const window = document.documentWindows[0];
+
+window.perspectiveName = "${perspectiveName}"
+
+const leaves = window.content().leaves().map(l => {
+    const task = l.value();
+    return {
+        id: task.id(),
+        name: task.name(),
+        flagged: task.flagged(),
+        deferDate: task.deferDate() ? task.deferDate().toString() : null,
+        dueDate: task.dueDate() ? task.dueDate().toString() : null,
+        dropped: task.dropped(),
+        tags: task.tags ? task.tags().map(tt => tt.name()) : []
+    };
+})
+
+return leaves;
+
+`);
+}
---
+++ src/perspectives.tsx
@@ -0,0 +1,45 @@
+import { usePromise } from "@raycast/utils";
+import { listPerspectiveTasks } from "./lib/api/list-perspectives-tasks";
+import { TaskList } from "./lib/components/task-list";
+import { List } from "@raycast/api";
+import { useState } from "react";
+import { listPerspectives } from "./lib/api/list-perspectives";
+
+export default function PerspectivesCommand() {
+  const [perspective, setPerspective] = useState<string>();
+  const { isLoading: perspectiveLoading, data: perspectives } = usePromise(() => listPerspectives());
+  const { isLoading, data, revalidate } = usePromise((name) => listPerspectiveTasks(name), [perspective]);
+
+  if (perspectiveLoading) {
+    return <List isLoading />;
+  }
+
+  const { customPerspectives, names } = perspectives!;
+
+  const customNames = customPerspectives.map((p) => p.name);
+  const builtInNames = names.filter((name) => !customNames.includes(name));
+
+  return (
+    <TaskList
+      isLoading={isLoading}
+      tasks={data}
+      title="Could do"
+      onTaskUpdated={revalidate}
+      accessories={
+        <List.Dropdown tooltip="Name of the perspective" onChange={setPerspective}>
+          <List.Dropdown.Section title="Built-in perspectives">
+            {builtInNames.map((n) => (
+              <List.Dropdown.Item key={n} title={n} value={n} />
+            ))}
+          </List.Dropdown.Section>
+
+          <List.Dropdown.Section title="Custom perspectives">
+            {perspectives?.customPerspectives.map((p) => (
+              <List.Dropdown.Item key={p.id} title={p.name} value={p.name} />
+            ))}
+          </List.Dropdown.Section>
+        </List.Dropdown>
+      }
+    />
+  );
+}
